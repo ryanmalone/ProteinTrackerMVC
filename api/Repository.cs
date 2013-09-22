c877cf60724db576b3e600715c3e33f5619c9f75
ryan.malone@blondgorilla.com <ryan.malone@blondgorilla.com>
2013-09-22 22:58:24 +0800

    first commit

---
+++ api/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ServiceStack.Redis;
+
+namespace ProteinTrackerMVC.api
+{
+
+    public interface  IRepository
+    {
+        long AddUser(string name, int goal);
+    }
+    public class Repository : IRepository
+    {
+        IRedisClientsManager RedisManager { get; set; }
+    
+        public Repository(IRedisClientsManager redisManager)
+        {
+            RedisManager = redisManager;
+        }
+
+        public long AddUser(string name, int goal)
+        {
+            using (var redisclient = RedisManager.GetClient())
+            {
+                var redisUsers = redisclient.As<User>();
+                var user = new User() {Name = name, Goal = goal, ID = redisUsers.GetNextSequence()};
+                redisUsers.Store(user);
+                return user.ID;
+                ;
+
+                ;
+            }
+        }
+    }
+}
---
+++ api/UserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+
+namespace ProteinTrackerMVC.api
+{
+    public class UserService : Service
+    {
+
+        public IRepository Repository { get; set; }
+
+        public object Post(AddUser request)
+        {
+
+            var id = Repository.AddUser(request.Name, request.Goal);
+
+            // add the user
+            return new AddUserResponse { UserID = id };
+        }
+    }
+
+    public class AddUserResponse
+    {
+        public long UserID { get; set; }
+    }
+
+    [Route("/users", "POST")]
+    public class AddUser
+    {
+        public string Name { get; set; }
+        public int Goal { get; set;}
+    }
+}
